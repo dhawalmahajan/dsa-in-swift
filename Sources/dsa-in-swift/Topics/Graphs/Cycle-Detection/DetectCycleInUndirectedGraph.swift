206e299cb3d14bf7920b3e919724d22ebc2d1621
Dhawal Mahajan <dhawalmahajan@users.noreply.github.com>
2026-05-13 11:44:26 +0530

    add bfs cycle detection approch

--- Sources/dsa-in-swift/Topics/Graphs/Cycle-Detection/DetectCycleInUndirectedGraph.swift
+++ Sources/dsa-in-swift/Topics/Graphs/Cycle-Detection/DetectCycleInUndirectedGraph.swift
@@ -44,14 +44,48 @@ func detectCycleDemoUsingDFS() {
 }
 
 //MARK: BFS Method
-private func detectCycle(adj:  [[Int]],inout visited: [Bool]) -> Bool {
-    
-    var queue:[(Int, Int)] = []
-    visited[0] = true
-    queue.append((0,-1))
-    while !queue.isEmpty {
-        let (node,parent) = queue.removeFirst() 
-        
+private func isCycleUsingBFS(_ v: Int,_ adj: [[Int]]) -> Bool {
+    var visited:[Bool] = Array(repeating: false, count: v)
+    for i in 0..<v {
+        if !visited[i] && detectCycle(vertex: i, adj: adj, visited: &visited) {
+            return true
+        }
     }
+    return false
 }
 
+private func detectCycle(vertex: Int, adj: [[Int]], visited:inout [Bool]) -> Bool {
+    var queue:[(Int, Int)] = []
+    visited[vertex] = true
+    queue.append((vertex,-1))
+    while !queue.isEmpty {
+        let (node,parent) = queue.removeFirst() 
+        for neighbour in adj[node] {
+            if parent == neighbour {
+                continue
+            }
+            if (visited[neighbour] == true) {
+                returnreturn true
+            }
+            visited[neighbour] = true
+            queue.append((neighbour, node))
+        }
+    }
+    return false
+}
+
+func detectCycleUsingBFS() {
+    let nodeCount = 4
+    let adj = [
+        [1, 2],    // Neighbors of node 0
+        [0, 2],    // Neighbors of node 1 (Cycle here: 0-1-2-0)
+        [0, 1, 3], // Neighbors of node 2
+        [2]        // Neighbors of node 3
+    ]
+    let result = isCycleUsingBFS(nodeCount, adj)
+    if result == true {
+        print("Cycle detected")
+    } else {
+        print("Cycle not detected")
+    }
+}
