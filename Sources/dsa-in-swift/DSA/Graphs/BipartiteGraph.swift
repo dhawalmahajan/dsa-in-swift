b7695c6fed8fd8c6a45d3c674eae0413e43ddfbe
Dhawal Mahajan <dhawal.mahajan1@gmail.com>
2026-05-31 11:27:14 +0530

    add dfs approch for bipartite graph

--- Sources/dsa-in-swift/DSA/Graphs/BipartiteGraph.swift
+++ Sources/dsa-in-swift/DSA/Graphs/BipartiteGraph.swift
@@ -5,7 +5,8 @@
 //  Created by Dhawal Mahajan on 30/05/26.
 //
 
-private func isBipartite(_ v: Int, adj: [[Int]]) -> Bool {
+//MARK: BFS
+private func isBipartiteUsingBFS(_ v: Int, adj: [[Int]]) -> Bool {
     var color = Array(repeating: -1, count: v)
     var q: [Int] = []
     for i in 0..<v {
@@ -31,6 +32,32 @@ private func isBipartite(_ v: Int, adj: [[Int]]) -> Bool {
     }
     return true
 }
+//MARK: DFS
+private func isBipartiteGraphUsingDFS(_ v: Int, adj: [[Int]]) -> Bool {
+    var color = Array(repeating: -1, count: v)
+    for i in 0..<v {
+        if color[i] == -1 {
+            color[i] = 0
+            return isBipartiteDFS(i, adj: adj, color: &color)
+        }
+    }
+    return true
+}
+private func isBipartiteDFS(_ node: Int, adj: [[Int]], color: inout [Int]) -> Bool {
+    for neighbour in adj[node] {
+        if(color[neighbour] == -1) {
+            color[neighbour] = (color[node] + 1) % 2
+            if !isBipartiteDFS(neighbour, adj: adj, color: &color) {
+                return false
+            }
+        } else {
+            if color[node] == color[neighbour] {
+                return false
+            }
+        }
+    }
+    return true
+}
 func bipartiteGraphDemo() {
     let v = 4
     let adj = [
@@ -39,5 +66,12 @@ func bipartiteGraphDemo() {
         [1, 3], // Neighbors of 2
         [0, 2]  // Neighbors of 3
     ]
-    print("Is Graph 1 Bipartite? \(isBipartite(v, adj: adj))")
+    let adj2 = [
+        [1, 2, 3], // Node 0 connects to 1, 2, and 3
+        [0, 2],    // Node 1 connects to 0 and 2
+        [0, 1, 3], // Node 2 connects to 0, 1, and 3
+        [0, 2]     // Node 3 connects to 0 and 2
+    ]
+    print("Is Graph 1 Bipartite using BFS? \(isBipartiteUsingBFS(v, adj: adj))")
+    print("Is Graph 1 Bipartite Using DFS? \(isBipartiteGraphUsingDFS(v, adj: adj))")
 }
